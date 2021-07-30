aa4728ab44bdc4476bb8560442b9081585ef7d00
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-07-30 09:25:36 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+Author: Bozo the Clown
+Date: 30 Jul 2021
+Description:
+Demo of pycharm with Git and Github
+'''
+
+
+def main():
+    print("Hello, My name is Bozo")
+
+
+if __name__ == '__main__':
+    main()
