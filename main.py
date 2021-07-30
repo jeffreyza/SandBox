7daff9fcfed7d941bb63b292fbf63e05912c83d2
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-07-30 09:30:10 +0800

    Add function to print stars

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ Demo of pycharm with Git and Github
 
 def main():
     print("Hello, My name is Bozo")
+    for i in range(1, 6):
+        print_stars(i)
+
+def print_stars(number):
+    print("*" * number)
 
 
 if __name__ == '__main__':
