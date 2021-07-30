f59f28cb5b82b55474e8ecf04f1d4265d57047ae
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-07-30 09:50:46 +0800

    Modified code to calculate the area of a circle

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ Description:
 Demo of pycharm with Git and Github
 '''
 
+from utilities import *
+
 
 def main():
     print("Hello, My name is Bozo")
     for i in range(1, 6):
         print_stars(i)
+
+    print("area of circle with radius of 5 units: {:.2f}\n".format(calculate_area_of_circle(5)))
+
 
 def print_stars(number):
     print("*" * number)
